891c8ca516c049e54a4cefe0f85e9949640e5ac1
Matthieu MOREL <matthieu.morel35@gmail.com>
2025-01-19 23:51:48 +0100

    [chore]: enable nolintlint
    Signed-off-by: Matthieu MOREL <matthieu.morel35@gmail.com>

--- exporter/datadogexporter/examples_test.go
+++ exporter/datadogexporter/examples_test.go
@@ -56,7 +56,6 @@ func TestExamples(t *testing.T) {
 			t.Setenv("DD_API_KEY", "aaaaaaaaa")
 			name := filepath.Join(folder, f.Name())
 			// https://github.com/open-telemetry/opentelemetry-collector-contrib/issues/33594
-			// nolint:staticcheck
 			_, err := otelcoltest.LoadConfigAndValidate(name, factories)
 			require.NoError(t, err, "All yaml config must validate. Please ensure that all necessary component factories are added in newTestComponents()")
 		})
@@ -86,7 +85,6 @@ func TestExamples(t *testing.T) {
 		require.NoError(t, f.Close())
 		defer os.RemoveAll(f.Name())
 		// https://github.com/open-telemetry/opentelemetry-collector-contrib/issues/33594
-		// nolint:staticcheck
 		_, err = otelcoltest.LoadConfigAndValidate(f.Name(), factories)
 		require.NoError(t, err, "All yaml config must validate. Please ensure that all necessary component factories are added in newTestComponents()")
 	})
